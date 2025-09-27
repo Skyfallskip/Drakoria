6362a503408931383b1fea0f14a8a79956e29116
Sk1p <mateuslirio3@gmail.com>
2025-09-27 18:23:54 -0300

    Começo do mapa V 0.1 (Dessa vez de verdade)

---
+++ Tiled_Map_Editor_Stuff/Tilesets_tsx/Basic_Tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Basic_Tileset" tilewidth="32" tileheight="32" tilecount="12" columns="4">
+ <image source="../../../../../Tiled_Map_Editor_Stuff\Tilesets_tsx\Tiles_Basicos.png" trans="ffffff" width="128" height="96"/>
+</tileset>
